ee69c86b53999b81d8242ed0abddbe010b411981
smlee212 <smmylee212@naver.com>
2024-12-04 14:49:39 +0900

    [feat] 프로젝트 생성

---
+++ src/main/java/com/wanted/challenge26/Challenge26Application.java
@@ -0,0 +1,13 @@
+package com.wanted.challenge26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge26Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/challenge26/Challenge26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.challenge26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
